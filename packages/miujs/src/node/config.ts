30b112d73864eced2058b86a698e9528a056f3d3
mast1ff <mast1ff.crux@gmail.com>
2022-04-23 04:12:00 +0900

    Version 0.1.52

--- packages/miujs/src/node/config.ts
+++ packages/miujs/src/node/config.ts
@@ -64,7 +64,7 @@ export async function loadConfig(root?: string, serverMode: ServerMode = "produc
       serverBuildPath = "api/index.js";
       break;
     case "netlify":
-      serverBuildPath = ".netlify/functions-internal/server.js";
+      serverBuildPath = "netlify/functions/server.js";
       break;
   }
   serverBuildDirectory = path.resolve(root, serverBuildDirectory);
